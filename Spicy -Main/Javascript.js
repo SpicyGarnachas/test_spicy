ee080d4661416c6b6e2c990c1fbce1afbfb62aa2
Diego Martínez <56555056+SpicyGarnachas@users.noreply.github.com>
2021-05-26 18:11:44 -0500

    Add files via upload

---
+++ Spicy -Main/Javascript.js
@@ -0,0 +1,9 @@
+function mouseOver() {
+    document.getElementById("path001").setAttribute("style", "fill: rgb(38.819885%,0%,0%);");
+    document.getElementById("path002").setAttribute("style", "fill: rgb(38.819885%,0%,0%);");
+}
+
+function mouseOut() {
+    document.getElementById("path001").setAttribute("style", "fill: black;");
+    document.getElementById("path002").setAttribute("style", "fill: black;");
+}
